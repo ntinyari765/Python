04e160179161f40170b9977f3c26b51ba9f9007d
ntinyari765 <winjoyntinyari765@gmail.com>
2025-07-29 20:25:40 +0300

    introduction to python

---
+++ intro.py
@@ -0,0 +1,33 @@
+"""PLP = "academy"""""
+
+
+# Student_Name = "Winjoy Ntinyari"
+# Student_id = 12345
+# Student_Age = 20
+# Fees_Clearance = True
+
+# print(Student_Name, Student_id, Student_Age, Fees_Clearance)
+
+#Lists
+# Languages = ["Python", "Java", "C++", "JavaScript", 1990]
+# print(Languages[2])
+
+# #tuples
+# fruits = ("apple", "banana", "cherry")
+# print(fruits)
+
+#Sets
+Set_A = {"Python", "Java", "C++", "JavaScript"}
+Set_B = {"Python", "Java", "C++", "JavaScript", 1990}
+print(Set_A)
+print(Set_B)
+print(Set_A.union(Set_B))  # Union of two sets
+print(Set_A.intersection(Set_B))  # Intersection of two sets
+
+#Dictionaries
+student_info = {
+   "name": "Winjoy Ntinyari",
+    "id": 12345,
+    "age": 20,
+    "fees_clearance": True}
+print(student_info)
